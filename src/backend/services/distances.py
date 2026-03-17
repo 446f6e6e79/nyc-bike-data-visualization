9c84ac47c8c9a1bdcfd839216b2e6e8f09e13760
446f6e6e79 <davidedona03@gmail.com>
2026-03-17 08:27:17 +0100

    Implement distances and weather services

--- src/backend/services/distances.py
+++ src/backend/services/distances.py
@@ -1,1 +1,39 @@
-#TODO: implement distance data loading and processing as in the rides data
+import polars as pl
+from datetime import datetime
+from pathlib import Path
+
+_distances_df: pl.DataFrame | None = None
+
+PROJECT_ROOT = Path(__file__).resolve().parents[3]
+BACKEND_ROOT = Path(__file__).resolve().parents[1]
+
+DATA_DIR = PROJECT_ROOT / "data"
+DISTANCES_DATA_DIR = DATA_DIR / "distances"
+TEST_DATA_DIR = BACKEND_ROOT / "tests" / "test_data"
+
+def load_distances_data(test=False) -> pl.DataFrame:
+    """
+    Load all historical distances data from the given directory into
+    a single DataFrame. The result is cached in memory after the first call using
+    a singleton pattern
+    """
+    global _distances_df
+    if _distances_df is not None:
+        return _distances_df
+
+    print("Loading distances data...")
+    start_time = datetime.now()
+
+    # TODO: should we implement a test dataset also for distances data?
+    if test:
+        # If we are in test mode, load the csv file from the committed test dataset
+        print("Test mode enabled: loading data from committed test dataset.")
+        _distances_df = pl.read_csv(str(TEST_DATA_DIR / "distances.csv"))
+    
+    else:
+        # Otherwise, scan all the parquet files
+        _distances_df = pl.scan_parquet(str(DISTANCES_DATA_DIR / "*.parquet"))
+
+    end_time = datetime.now()
+    print(f"Distances data loaded successfully in {end_time - start_time}.")
+    return _distances_df
--- src/backend/services/weather.py
+++ src/backend/services/weather.py
@@ -1,1 +1,39 @@
-#TODO: implement weather data loading and processing as in the rides data
+import polars as pl
+from datetime import datetime
+from pathlib import Path
+
+_weather_df: pl.DataFrame | None = None
+
+PROJECT_ROOT = Path(__file__).resolve().parents[3]
+BACKEND_ROOT = Path(__file__).resolve().parents[1]
+
+DATA_DIR = PROJECT_ROOT / "data"
+WEATHER_DATA_DIR = DATA_DIR / "weather"
+TEST_DATA_DIR = BACKEND_ROOT / "tests" / "test_data"
+
+def load_weather_data(test=False) -> pl.DataFrame:
+    """
+    Load all historical weather data from the given directory into
+    a single DataFrame. The result is cached in memory after the first call using
+    a singleton pattern
+    """
+    global _weather_df
+    if _weather_df is not None:
+        return _weather_df
+
+    print("Loading weather data...")
+    start_time = datetime.now()
+
+    # TODO: should we implement a test dataset also for weather data?
+    if test:
+        # If we are in test mode, load the csv file from the committed test dataset
+        print("Test mode enabled: loading data from committed test dataset.")
+        _weather_df = pl.read_csv(str(TEST_DATA_DIR / "weather.csv"))
+    
+    else:
+        # Otherwise, scan all the parquet files
+        _weather_df = pl.scan_parquet(str(WEATHER_DATA_DIR / "*.parquet"))
+
+    end_time = datetime.now()
+    print(f"Weather data loaded successfully in {end_time - start_time}.")
+    return _weather_df
